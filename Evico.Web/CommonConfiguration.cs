e296027645791deb20f5759bc77c634d9abe91b0
Nikita Lyadov (Anchovy) <hakker990@gmail.com>
2022-10-10 16:11:35 +0500

    Replace with env variable

--- Evico.Web/CommonConfiguration.cs
+++
@@ -1,6 +0,0 @@
-namespace Evico.Web;
-
-public class CommonConfiguration
-{
-    public string ApiUrl { get; set; } = string.Empty;
-}
--- Evico.Web/Program.cs
+++ Evico.Web/Program.cs
@@ -1,7 +1,4 @@
-using Evico.Web;
-
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.Configure<CommonConfiguration>(builder.Configuration.GetSection("CommonConfiguration"));
 
 // Add services to the container.
 
