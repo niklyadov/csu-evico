d59c5e607ef17869ef1c5a2165fe926ea4479e54
Nikita Lyadov (Anchovy) <hakker990@gmail.com>
2022-10-22 16:46:42 +0500

    Update ApplicationContext.cs

--- Evico/ApplicationContext.cs
+++ Evico/ApplicationContext.cs
@@ -5,13 +5,17 @@ namespace Evico;
 
 public class ApplicationContext : DbContext
 {
-    public virtual DbSet<EntityRecord> EntityRecords { get; set; }
-    public virtual DbSet<PlaceRecord> PlaceRecords { get; set; }
+    public virtual DbSet<EntityRecord> Entities { get; set; }
+    public virtual DbSet<PlaceRecord> Places { get; set; }
+    public virtual DbSet<ReviewRecord> Reviews { get; set; }
+    public virtual DbSet<ProfileRecord> Profiles { get; set; }
+    public virtual DbSet<EventCategoryRecord> EventCategories { get; set; }
+    public virtual DbSet<PlaceCategoryRecord> PlaceCategories { get; set; }
     
     public ApplicationContext(DbContextOptions options) : base(options)
     {
-        //Database.EnsureDeleted();
-        //Database.EnsureCreated();
+        Database.EnsureDeleted();
+        Database.EnsureCreated();
     }
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
