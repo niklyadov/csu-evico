92e01306be1caf37e7726b33d17e3f4583012db9
Nikita Lyadov (Anchovy) <hakker990@gmail.com>
2022-11-02 20:09:59 +0500

    Delete unused

--- Evico.Web/Program.cs
+++ Evico.Web/Program.cs
@@ -1,10 +1,7 @@
-using Evico.Web.Services;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddScoped<VkAuthService>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
