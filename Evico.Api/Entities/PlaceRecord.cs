2825ced315320e6152f76e173553c9e639ff018c
Nikita Lyadov (Anchovy) <hakker990@gmail.com>
2022-11-08 22:44:23 +0500

    Parent place for place

--- Evico.Api/Entities/PlaceRecord.cs
+++ Evico.Api/Entities/PlaceRecord.cs
@@ -1,17 +1,23 @@
+using Newtonsoft.Json;
+
 namespace Evico.Api.Entities;
 
 public record PlaceRecord : EntityRecord
 {
-    public double LocationLatitude { get; set; } = default!;
+    public double LocationLatitude { get; set; }
 
-    public double LocationLongitude { get; set; } = default!;
+    public double LocationLongitude { get; set; }
 
     public long OwnerId { get; set; }
     public ProfileRecord Owner { get; set; } = default!;
-    public string Name { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
+    public String Name { get; set; } = String.Empty;
+    public String Description { get; set; } = String.Empty;
     public ExternalPhotoRecord? Photo { get; set; } = null;
     public virtual List<ExternalPhotoRecord> Photos { get; set; } = new();
+    [JsonIgnore]
     public virtual List<PlaceReviewRecord> Reviews { get; set; } = new();
     public virtual List<PlaceCategoryRecord> Categories { get; set; } = new();
+    
+    public long? ParentId { get; set; }
+    public PlaceRecord? Parent { get; set; }
 }
