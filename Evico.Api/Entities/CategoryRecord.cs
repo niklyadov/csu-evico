71852f14f38736d6b90e374bf6ccafe82339a64e
Nikita Lyadov (Anchovy) <hakker990@gmail.com>
2022-11-08 21:10:00 +0500

    Parent category id, fix json serialize parent

--- Evico.Api/Entities/CategoryRecord.cs
+++ Evico.Api/Entities/CategoryRecord.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace Evico.Api.Entities;
 
@@ -7,5 +8,7 @@ public abstract record CategoryRecord : EntityRecord
 {
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public CategoryRecord? Parent { get; set; } = null;
+    public long? ParentId { get; set; }
+    [JsonIgnore]
+    public CategoryRecord? Parent { get; set; }
 }
