f8db84f6da6a3931eee3f2cb217cb20408415e2b
Nikita Lyadov (Anchovy) <hakker990@gmail.com>
2022-12-21 16:46:58 +0500

    Update config.js

--- Evico.Web/ClientApp/src/config.js
+++ Evico.Web/ClientApp/src/config.js
@@ -5,8 +5,7 @@ const config = {
     authBearerToken: 'authBearerToken',
     authRefreshToken: 'authRefreshToken',
     redirect_uri_auth: process.env.REACT_APP_REDIRECT_URI_AUTH,
-    bearerToken: "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJJZCI6IjcyZWM1YjVkLTBkOGItNDJiOS05ZjI1LTBkYWFlNmE5YWY5MyIsInN1YiI6IjEiLCJuYW1lIjoiTmlraXRhSG90RG9nIiwiZW1haWwiOiIiLCJqdGkiOiIyMjk1NWUyOC1lNWExLTRkYmEtOTdkMi05ZjQ3OWRhZTIwZmIiLCJuYmYiOjE2Njg1ODg5MTcsImV4cCI6MTY2OTE5MzcxNywiaWF0IjoxNjY4NTg4OTE3LCJpc3MiOiJDU1UtRVZJQ08iLCJhdWQiOiJDU1UtRVZJQ08ifQ.7zFDS1E57J96T6nIHUcRNnc73IS-RpLYl9JwaopLYtECuHUgnehH9P7s52sn6hvrYUPuDPR1KvThq3r5yvWvvg",
-
+    bearerToken: localStorage.getItem('authBearerToken')
 };
 
 export default config;
